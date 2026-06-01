375eda840bfcf344a1d29857a2142ed26eabcb11
ELBGG <github-actions[bot]@users.noreply.github.com>
2026-06-01 18:26:55 -0500

    reto 4 completado

--- examples/levy.c
+++ examples/levy.c
@@ -1,45 +1,48 @@
 #include <math.h>
 #include "../turtlec.h"
 
-void colorLevel(Turtle *turtle, int depth){
-	switch(depth % 6){
-		case 0: turtleSetColor(turtle, 255, 0, 0);
-			break;
-		case 1: turtleSetColor(turtle, 255, 164, 0);
-                        break;
-		case 2: turtleSetColor(turtle, 255, 255, 0);
-                        break;
-		case 3: turtleSetColor(turtle, 0, 255, 0);
-                        break;
-		case 4: turtleSetColor(turtle, 0, 0, 255);
-                        break;
-		case 5: turtleSetColor(turtle, 127, 0, 127);
-                        break;
-	}
+void colorLevel(Turtle *turtle, int level){
+    switch(level % 9){
+        case 0: turtleSetColor(turtle, 255, 0, 0);
+                break;
+        case 1: turtleSetColor(turtle, 255, 128, 0);
+                break;
+        case 2: turtleSetColor(turtle, 255, 255, 0);
+                break;
+        case 3: turtleSetColor(turtle, 128, 255, 0);
+                break;
+        case 4: turtleSetColor(turtle, 0, 255, 0);
+                break;
+        case 5: turtleSetColor(turtle, 0, 255, 255);
+                break;
+        case 6: turtleSetColor(turtle, 0, 0, 255);
+                break;
+        case 7: turtleSetColor(turtle, 127, 0, 255);
+                break;
+        case 8: turtleSetColor(turtle, 255, 0, 255);
+                break;
+    }
 }
 
-void levy(Turtle *turtle, float length, int depth){
-	colorLevel(turtle, depth);
+void levy(Turtle *turtle, float length, int depth, int level){
+    if(depth == 0){
+        colorLevel(turtle, level);
+        turtleForward(turtle, length);
+        return;
+    }
 
-	if(depth == 0){
-		turtleForward(turtle, length);
-		return;
-	}
-
-	turtleLeft(turtle, 45);
-	levy(turtle, length / sqrt(2) , depth - 1);
-
-	turtleRight(turtle, 90);
-	levy(turtle, length / sqrt(2) , depth - 1);
-
-	turtleLeft(turtle, 45);
+    turtleLeft(turtle, 45);
+    levy(turtle, length / sqrt(2), depth - 1, level * 2);
+    turtleRight(turtle, 90);
+    levy(turtle, length / sqrt(2), depth - 1, level * 2 + 1);
+    turtleLeft(turtle, 45);
 }
 
 void draw(Turtle *turtle){
-	levy(turtle, 80.0f, 9);
+    levy(turtle, 150.0f, 9, 0);
 }
 
 int main(void){
-	turtleRun(draw, 800, 800, "Levy C");
-	return 0;
+    turtleRun(draw, 800, 800, "Levy C");
+    return 0;
 }
